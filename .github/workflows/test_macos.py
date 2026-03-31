d6824e0559eeb844c3c007d87de2303eab6ecae7
Bruno von Bruening <bruno.vonbruening@outlook.de>
2026-03-31 13:58:17 +0100

    formated test_macos.py

--- .github/workflows/test_macos.py
+++ .github/workflows/test_macos.py
@@ -7,40 +7,39 @@ on:
   pull_request:
 
 jobs:
-test-macos:
-runs-on: macos-14
+    test-macos:
+        runs-on: macos-14
 
-```
-steps:
-  - name: Checkout repo
-    uses: actions/checkout@v4
+        steps:
+          - name: Checkout repo
+            uses: actions/checkout@v4
 
-  - name: Install Miniconda
-    uses: conda-incubator/setup-miniconda@v3
-    with:
-      auto-update-conda: true
-      python-version: 3.11
-      activate-environment: myenv  # optional: auto-activate after creation
+          - name: Install Miniconda
+            uses: conda-incubator/setup-miniconda@v3
+            with:
+              auto-update-conda: true
+              python-version: 3.11
+              activate-environment: myenv  # optional: auto-activate after creation
 
-  - name: Create conda environment from YAML
-    run: conda env create -f install/qcpAPI_env.yaml -n test_env
+          - name: Create conda environment from YAML
+            run: conda env create -f install/qcpAPI_env.yaml -n test_env
 
-  - name: Activate environment
-    shell: bash
-    run: |
-      conda activate test_env
-      python --version
-      conda list
+          - name: Activate environment
+            shell: bash
+            run: |
+              conda activate test_env
+              python --version
+              conda list
 
-  - name: Install your package into environment
-    shell: bash
-    run: |
-      conda activate test_env
-      pip install .
+          - name: Install your package into environment
+            shell: bash
+            run: |
+              conda activate test_env
+              pip install .
 
-  - name: Run test script
-    shell: bash
-    run: |
-      conda activate test_env
-      cd tests/molpol_run
-      python test.py
+          - name: Run test script
+            shell: bash
+            run: |
+              conda activate test_env
+              cd tests/molpol_run
+              python test.py
