7d7b0f80447e6f7922c02b030a8ca571ce5eeec9
Bruno von Bruening <bruno.vonbruening@outlook.de>
2026-04-01 00:29:51 +0100

    fixed clone script

--- src/util/environment.py
+++ src/util/environment.py
@@ -5,7 +5,7 @@ from qcp_global_utils.environment.file_handling import temporary_file, link_file
 from qcp_global_utils.environment.conda_env import get_conda_base, get_python_from_conda_env
 from qcp_global_utils.shell_processes.execution import run_shell_command
 
-
+import os
 
 def get_enviornment_variable(variable_name:str, critical:bool=True) -> str:
     if not variable_name in os.environ.keys():
