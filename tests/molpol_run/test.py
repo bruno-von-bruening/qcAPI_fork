715bc5c56cf044e319c6918137ef246bdc0acc8f
Bruno von Bruening <bruno@cn292.htc.esc.qmul>
2026-05-22 21:17:18 +0100

    fixed wrong test file

--- tests/molpol_run/test.py
+++ tests/molpol_run/test.py
@@ -79,7 +79,7 @@ def main(config_file):
     lots_file=lots_wider_file
     lots_file=lots_cc_hyb_df_file
 
-    molpol_file=os.path.join(test_dir,'supplementary_files','molpol','molpol.yaml')
+    molpol_file=os.path.join(test_dir,'supplementary_files','molpol','molpol_ff.yaml')
 
     assert os.path.isfile(molpol_file), f"Not a file: {molpol_file}"
     proc=start_server(config_file)
